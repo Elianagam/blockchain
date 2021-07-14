b0c168128bed8e5e202c7a119dad3eeffeaea205
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-07-13 22:46:07 -0300

    Use pattern matching rather than if

--- src/node/main.rs
+++ src/node/main.rs
@@ -8,20 +8,22 @@ use std::str;
 
 const LEADER_ADDR: &str = "127.0.0.1:8000";
 
-fn run_bully_as_non_leader() {
+fn run_bully_as_non_leader(mut blockchain: Vec<String>) {
 	// Let the OS to pick one addr + port for us
 	let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
 
-	socket.send_to("REGISTRAR".as_bytes(), LEADER_ADDR).unwrap();
+	socket.send_to("register\n".as_bytes(), LEADER_ADDR).unwrap();
 
 	for _ in 0..3 {
 		let mut buf = [0; 128];
 		let (size, from) = socket.recv_from(&mut buf).unwrap();
-		println!("Recibido {:?}", str::from_utf8(&buf).unwrap());
+		let msg = str::from_utf8(&buf).unwrap();
+		println!("Recibido {:?}", &msg);
+		blockchain.push(msg.to_string());
 	}
 }
 
-fn run_bully_as_leader() {
+fn run_bully_as_leader(mut blockchain: Vec<String>) {
 	println!("Soy el líder!");
 
 	let mut other_nodes: Vec<SocketAddr> = vec!();
@@ -31,39 +33,39 @@ fn run_bully_as_leader() {
 	let mut message_count = 3;
 
 	loop {
-		let mut buf = [0; 10];
+		let mut buf = [0; 128];
 		let (size, from) = socket.recv_from(&mut buf).unwrap();
-		println!("recibido {:?}", str::from_utf8(&buf));
 
-		if str::from_utf8(&buf).unwrap() == "REGISTRAR\0" {
-			println!("Registrando nodo {}", from);
-			if !&other_nodes.contains(&from) {
-				other_nodes.push(from);
+		match str::from_utf8(&buf).unwrap() {
+			"register\n" => {
+				println!("Registrando nodo: {}", from);
+				if !&other_nodes.contains(&from) {
+					other_nodes.push(from);
+				}
+			},
+			msg => {
+				println!("Propagando cambios {:?} al resto de los nodos", msg);
+				for node in &other_nodes {
+					socket.send_to(msg.as_bytes(), node).unwrap();
+				}
+				blockchain.push(msg.to_string());
+				message_count -= 1;
 			}
-			continue;
-		} 
-
-		println!("Propagando cambios al resto de los nodos");
-
-		message_count -= 1;
-
-		for node in &other_nodes {
-			socket.send_to("NUEVO CAMBIO".as_bytes(), node).unwrap();
 		}
 	}
 }
 
 fn run_bully_thread(iamleader: bool) -> () {
-	if iamleader {
-		run_bully_as_leader()
+	let blockchain: Vec<String> = vec!(); 
+
+	match iamleader {
+		true => run_bully_as_leader(blockchain),
+		false => run_bully_as_non_leader(blockchain)
 	}
-	run_bully_as_non_leader()
 }
 
 
 fn main() -> Result<(), ()> {
-	let address = "0.0.0.0:10000".to_string();
-
 	let args: Vec<String> = env::args().collect();
 
 	let iamleader: bool = args.len() > 1 && args[1] == "--leader";
@@ -73,6 +75,8 @@ fn main() -> Result<(), ()> {
 	t.join();
 
 	return Ok(());
+
+	let address = "0.0.0.0:10000".to_string();
 
 	let mut socket = TcpStream::connect(address).unwrap();
 	println!("Conectando. Ingrese texto");
