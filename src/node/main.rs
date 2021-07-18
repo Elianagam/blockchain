ab381a6c0df9997ffaccb9c122c0b49c4636e544
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-07-18 20:01:03 -0300

    Agrega objeto Blockchain, Block

--- src/node/main.rs
+++ src/node/main.rs
@@ -5,6 +5,10 @@ use std::process;
 use std::str;
 
 mod node;
+mod blockchain;
+
+use blockchain::{Blockchain, Block};
+
 
 const LEADER_ADDR: &str = "127.0.0.1:8000";
 const DUMMY_MSG: &str = "testing";
@@ -25,7 +29,7 @@ fn decode_from_bytes(payload: Vec<u8>) -> String {
     data.to_string()
 }
 
-fn run_bully_as_non_leader(mut blockchain: Vec<String>) {
+fn run_bully_as_non_leader(mut blockchain: Blockchain) {
 	// Let the OS to pick one addr + port for us
 	let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
 
@@ -44,13 +48,13 @@ fn run_bully_as_non_leader(mut blockchain: Vec<String>) {
 
 		println!("Recibido {}", &msg);
 
-		blockchain.push(msg);
+		blockchain.add(Block{ data: msg });
 	}
 
     println!("Blockchain final: {:?}", blockchain);
 }
 
-fn run_bully_as_leader(mut blockchain: Vec<String>) {
+fn run_bully_as_leader(mut blockchain: Blockchain) {
 	println!("Soy el líder!");
 
 	let mut other_nodes: Vec<SocketAddr> = vec!();
@@ -79,15 +83,15 @@ fn run_bully_as_leader(mut blockchain: Vec<String>) {
 				for node in &other_nodes {
 					socket.send_to(&encode_to_bytes(msg), node).unwrap();
 				}
-				blockchain.push(msg.to_string());
+				blockchain.add(Block{ data: msg.to_string() });
                 propagated_msgs += 1;
 			}
 		}
 	}
 }
 
-fn run_bully_thread(iamleader: bool) -> () {
-	let blockchain: Vec<String> = vec!(); 
+fn run_bully_thread(iamleader: bool) -> () { 
+    let blockchain = Blockchain::new();
 
 	match iamleader {
 		true => run_bully_as_leader(blockchain),
@@ -96,7 +100,7 @@ fn run_bully_thread(iamleader: bool) -> () {
 }
 
 fn usage() -> i32 {
-	println!("Usage: cargo r --bin node <id> <ip_address> [--leader]");
+	println!("Usage: cargo r --bin node [--leader]");
 	return -1;
 }
 
@@ -104,14 +108,11 @@ fn usage() -> i32 {
 fn main() -> Result<(), ()> {
 	let args: Vec<String> = env::args().collect();
 
-    if args.len() != 1 {
+    if args.len() > 2 {
         process::exit(usage());
     }
 
-	let id = &args[1];
-	let address = &args[2];
-
-	let iamleader: bool = args.len() > 3 && args[3] == "--leader";
+	let iamleader: bool = args.len() > 1 && args[1] == "--leader";
 	let t = thread::spawn(move || run_bully_thread(iamleader));
 
     let mut node = node::Node::new();
