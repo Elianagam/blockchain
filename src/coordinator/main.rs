9f5b7f68d1015fd3058aa3234bff371ad532a347
Eliana Gamarra <eliana.gamarra@mercadolibre.com>
2021-07-19 09:11:36 -0300

    Deleted comments

--- src/coordinator/main.rs
+++ src/coordinator/main.rs
@@ -29,19 +29,3 @@ fn main() -> Result<(), ()> {
 
 	Ok(())
 }
-
-/*
-let address = "0.0.0.0:10000".to_string();
-
-	let listener = TcpListener::bind(address).unwrap();
-
-	// get client
-	let (stream, _addr) = listener.accept().unwrap();
-	let reader = BufReader::new(stream);
-	let mut lines = reader.lines();
-
-	while let Some(line) = lines.next() {
-		println!("Recibido: {}", line.unwrap());
-	}
-
-	*/
