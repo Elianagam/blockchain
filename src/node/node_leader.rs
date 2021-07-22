c64663fa3e9988d57962e2f71b35f980b55fe6d3
Elianagam <elianagam2@gmail.com>
2021-07-22 14:35:36 -0300

    Send address all nodes after registrer

--- src/node/node_leader.rs
+++ src/node/node_leader.rs
@@ -10,13 +10,15 @@ const REGISTER_MSG: &str = "register";
 const NEW_NODE: &str = "new_node";
 const END: &str = "-";
 
-fn send_all_addr(other_nodes: Vec<SocketAddr>, from: SocketAddr, socket: UdpSocket) {
-    socket.send_to(&Encoder::encode_to_bytes(NEW_NODE), from).unwrap();
-    for node in &other_nodes {
-        let addr = format!("{}", node);
-        socket.send_to(&Encoder::encode_to_bytes(&addr), from).unwrap();
+fn send_all_addr(other_nodes: Vec<SocketAddr>, socket: UdpSocket) {
+    for node_conected in &other_nodes {
+        socket.send_to(&Encoder::encode_to_bytes(NEW_NODE), node_conected).unwrap();
+        for node_addr in &other_nodes {
+            let addr = format!("{}", node_addr);
+            socket.send_to(&Encoder::encode_to_bytes(&addr), node_conected).unwrap();
+        }
+        socket.send_to(&Encoder::encode_to_bytes(END), node_conected).unwrap();
     }
-    socket.send_to(&Encoder::encode_to_bytes(END), from).unwrap();
 }
 
 pub fn run_bully_as_leader(mut blockchain: Blockchain) {
@@ -43,12 +45,12 @@ pub fn run_bully_as_leader(mut blockchain: Blockchain) {
                 println!("Registrando nodo: {}", from);
                 if !&other_nodes.contains(&from) {
                     other_nodes.push(from);
+                    send_all_addr(other_nodes.clone(), socket.try_clone().unwrap());
                 }
 
             }
             msg => {
                 println!("Propagando cambios {:?} al resto de los nodos", msg);
-                send_all_addr(other_nodes.clone(), from, socket.try_clone().unwrap());
                 for node in &other_nodes {
                     socket.send_to(&Encoder::encode_to_bytes(msg), node).unwrap();
 
