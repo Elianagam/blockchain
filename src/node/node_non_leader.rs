5695981ba6c3f930794ac389f68be71590ebcbef
Elianagam <elianagam2@gmail.com>
2021-07-24 15:49:56 -0300

    Send blockchain to new_node

--- src/node/node_non_leader.rs
+++ src/node/node_non_leader.rs
@@ -1,8 +1,21 @@
-use crate::messages::{PING_MSG, REGISTER_MSG, NEW_NODE, END};
+use crate::messages::{PING_MSG, REGISTER_MSG, NEW_NODE, END, BLOCKCHAIN_MSG};
 use crate::encoder::Encoder;
 use crate::blockchain::{Block, Blockchain};
 
 use std::net::UdpSocket;
+
+fn recv_blockchain(mut blockchain: Blockchain, socket: UdpSocket) -> Blockchain {
+    loop {
+        let mut buf = [0; 128];
+        let (_, _) = socket.recv_from(&mut buf).unwrap();
+        let block = Encoder::decode_from_bytes(buf.to_vec());
+        if block == END {
+            break;
+        }
+        blockchain.add(Block { data: block });
+    }
+    blockchain
+}
 
 fn recv_all_addr(mut other_nodes: Vec<String>, socket: UdpSocket) -> Vec<String> {
     loop {
@@ -38,12 +51,18 @@ pub fn run_bully_as_non_leader(socket: UdpSocket, mut blockchain: Blockchain, le
         let msg = Encoder::decode_from_bytes(buf.to_vec());
         println!("{:?}", other_nodes);
 
-        if msg.as_str() == NEW_NODE {
-            other_nodes = recv_all_addr(other_nodes.clone(), socket.try_clone().unwrap());
-        } else {
-            println!("Recibido {}", &msg);
-            blockchain.add(Block { data: msg });
-            println!("{}", blockchain);
+        match msg.as_str() {
+            NEW_NODE =>{
+                other_nodes = recv_all_addr(other_nodes.clone(), socket.try_clone().unwrap());
+            }
+            BLOCKCHAIN_MSG => {
+                blockchain = recv_blockchain(blockchain, socket.try_clone().unwrap());
+            }
+            msg => {
+                println!("Recibido {}", &msg);
+                blockchain.add(Block { data: msg.to_string() });
+                println!("{}", blockchain);
+            }
         }
     }
 
