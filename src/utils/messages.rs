2c75c0fcf9f0b4bf90e14e7f73ae5710c2de8bfd
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-07-25 12:19:18 -0300

    ignore unused code

--- src/utils/messages.rs
+++ src/utils/messages.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 pub const PING_MSG: &str = "ping";
 pub const REGISTER_MSG: &str = "register";
 pub const NEW_NODE: &str = "new_node";
@@ -8,4 +10,3 @@ pub const NEW_NODE_MSG: &str = "discover\n";
 pub const DISCONNECT_MSG: &str = "";
 pub const DISCOVER_MSG: &str = "discover\n";
 pub const BLOCKCHAIN_MSG: &str = "blockchain";
-
