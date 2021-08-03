49d0f2617f86f7fb2b64c819ce6306587c1e1073
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2021-08-02 23:58:26 -0300

    Gracefull ending

--- src/utils/messages.rs
+++ src/utils/messages.rs
@@ -2,7 +2,7 @@ pub const END: &str = "-";
 pub const BLOCKCHAIN: &str = "blockchain";
 pub const CLOSE: &str = "close";
 pub const WHO_IS_LEADER: &str = "who_is_leader";
-pub const SHOW_BLOCKCHAIN: &str = "show_blockchain";
+pub const NOOP_MSG: &str = "no_op";
 
 // Mensaje devuelto por el lider cuando esta ok el recibo del dato
 pub const ACK_MSG: &str = "ack";
